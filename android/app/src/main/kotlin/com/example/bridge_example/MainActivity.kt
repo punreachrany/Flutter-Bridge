05c7350b01f0028e7a4d5aec1c76d4cc34592340
Punreach Rany <punreach.rany@yahoo.com>
2022-01-16 21:15:38 +0900

    first

---
+++ android/app/src/main/kotlin/com/example/bridge_example/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.bridge_example
+
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterView
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+import java.lang.reflect.Method
+
+class MainActivity: FlutterActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "battery").setMethodCallHandler {
+            call, result ->
+            // Note: this method is invoked on the main thread.
+            // TODO
+            if(call.method == "getBatteryPercentage") {
+                result.success(10)
+            } else {
+                result.error("424","Please check", "Wrong Implementation")
+            }
+
+        }
+    }
+
+}
